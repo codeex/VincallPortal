986b89d305d0311f14d2d4c8990022854f98b1b7
Raymond <Raymond.Zhang@comm100.com>
2022-05-20 17:47:12 +0800

    add release cmd

---
+++ dev/release.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const shelljs = require("shelljs");
+// const rimraf = require("rimraf");
+
+// const exec = (cmd) => {
+//   const res = shelljs.exec(cmd);
+//   if (res.code !== 0) {
+//     throw new Error(`exec "${cmd}" failed.`);
+//   }
+// };
+
+const release = () => {
+  const buildDir = path.resolve(__dirname, "../build/*");
+  const distDir = path.resolve(__dirname, "../dist/vincall");
+  shelljs.rm("-rf", distDir);
+  shelljs.mkdir("-p", distDir);
+  shelljs.mv(buildDir, distDir);
+};
+
+release();
