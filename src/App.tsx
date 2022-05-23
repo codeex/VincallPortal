56217237a6fb19a9432fba0daf79bcc7956f2eaf
Joanna <joanna.huang@comm100.com>
2022-05-23 11:19:47 +0800

    update api domain

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export const App = function () {
   return (
     <Admin
       title="Vin Call"
-      dataProvider={dataProviderFactory("http://localhost:4000")}
+      dataProvider={dataProviderFactory("http://voipapi.comm100dev.io")}
       loginPage={Login}
       layout={Layout}
       theme={lightTheme}
