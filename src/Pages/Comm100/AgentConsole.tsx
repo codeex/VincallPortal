c7fb40e3088d305dc873f46f05fb0cc01ed79df6
Raymond <Raymond.Zhang@comm100.com>
2022-05-31 19:36:18 +0800

    add submenu.

---
+++ src/Pages/Comm100/AgentConsole.tsx
@@ -0,0 +1,10 @@
+import { EmbeddedPage } from "./EmbeddedPage";
+
+export const AgentConsole = () => {
+  return (
+    <EmbeddedPage
+      title="Agent Console"
+      installCodeKeyName="Comm100 Agent Console Js Code"
+    />
+  );
+};
---
+++ src/Pages/Comm100/ControlPanel.tsx
@@ -0,0 +1,10 @@
+import { EmbeddedPage } from "./EmbeddedPage";
+
+export const ControlPanel = () => {
+  return (
+    <EmbeddedPage
+      title="Control Panel"
+      installCodeKeyName="Comm100 Control Panel Js Code"
+    />
+  );
+};
