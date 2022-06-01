dbb6a2cc1d1065109f72b9fe6f0d9f2eaad2cb52
Joanna <joanna.huang@comm100.com>
2022-06-01 20:36:19 +0800

    connect EnvConfig

--- src/EnvConfig.js
+++ src/EnvConfig.js
@@ -1,4 +1,6 @@
 export const EnvConfig = {
   serverUrl: "https://apitest.vincall.net",
+  routeUrl: "https://voiproute.testing.comm100dev.io",
+  redirectUrlDomain: "https://apitest.vincall.net",
   useMockServer: false,
 };
