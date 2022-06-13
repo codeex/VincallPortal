3da9afc44e3e27aeb0cd92975431b334660680b4
Joanna Huang <Joanna.Huang@comm100.com>
2022-06-13 07:21:59 +0000

    clear useless code in CMenuItem.tsx

--- src/Layout/CMenuItem.tsx
+++ src/Layout/CMenuItem.tsx
@@ -1,15 +1,6 @@
 import * as React from "react";
-import { ReactElement, ReactNode } from "react";
-import {
-  List,
-  MenuItem,
-  ListItemIcon,
-  Typography,
-  Collapse,
-  Tooltip,
-} from "@mui/material";
-import ExpandMore from "@mui/icons-material/ExpandMore";
-import { useTranslate, useSidebarState, MenuItemLink } from "react-admin";
+import { ReactElement } from "react";
+import { MenuItemLink } from "react-admin";
 
 interface Props {
   to: string;
